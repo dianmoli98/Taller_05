5841901a170b6ab6779eceb9d2d4cc89c862a093
J.D. Vallejo <juadival@espol.edu.ec>
2019-07-25 13:01:00 -0500

    Creados archivos y paquetes por patrón

--- TallerIntegradorPatrones/src/ChainOfResponsibility/Manejador.java
+++ TallerIntegradorPatrones/src/ChainOfResponsibility/Manejador.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Patrones;
+package ChainOfResponsibility;
 
 public class Manejador
 {
--- TallerIntegradorPatrones/src/Singleton/AtmUK.java
+++ TallerIntegradorPatrones/src/Singleton/AtmUK.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Patrones;
+package Singleton;
 
 import java.util.ArrayList;
 import java.util.Currency;
@@ -45,8 +45,8 @@ public class AtmUK {
     //Dentro de las transacciones se debe llamar al ATM para hacer el retiro o deposito de la cuenta correspondiente
     public static void transaction(Account cuenta){
         // here is where most of the work is
-        int choice; 
-        System.out.println("Please select an option"); 
+        int choice;
+        System.out.println("Please select an option");
         System.out.println("1. Withdraw");
         System.out.println("2. Deposit");
         System.out.println("3. Balance");
@@ -54,11 +54,11 @@ public class AtmUK {
         choice = in.nextInt();
         switch(choice){
             case 1:
-                float amount; 
-                System.out.println("Please enter amount to withdraw: "); 
+                float amount;
+                System.out.println("Please enter amount to withdraw: ");
                 amount = in.nextFloat();
                 if(amount > cuenta.getAmount() || amount == 0){
-                    System.out.println("You have insufficient funds\n\n"); 
+                    System.out.println("You have insufficient funds\n\n");
                     anotherTransaction(cuenta); // ask if they want another transaction
                 } else {
                     // Todo: verificar que se puede realizar el retiro del atm
@@ -69,31 +69,31 @@ public class AtmUK {
 
                     // Todo: Mostrar resumen de transacción o error
                     // "You have withdrawn "+amount+" and your new balance is "+balance;
-                    anotherTransaction(cuenta); 
+                    anotherTransaction(cuenta);
                 }
-            break; 
+            break;
             case 2:
-                    // option number 2 is depositing 
-                    float deposit; 
-                    System.out.println("Please enter amount you would wish to deposit: "); 
+                    // option number 2 is depositing
+                    float deposit;
+                    System.out.println("Please enter amount you would wish to deposit: ");
                     deposit = in.nextFloat();
                     // Todo: actualizar tanto la cuenta como el atm
-                    
+
                     // Todo: Mostrar resumen de transacción o error
                     // "You have withdrawn "+amount+" and your new balance is "+balance;
                     anotherTransaction(cuenta);
-            break; 
+            break;
             case 3:
                     // Todo: mostrar el balance de la cuenta
                     // "Your balance is "+balance
-                    anotherTransaction(cuenta); 
+                    anotherTransaction(cuenta);
             break;
             case 4:
                     // Todo: mostrar el balance del ATM con los billetes en cada manejador
-                    anotherTransaction(cuenta); 
+                    anotherTransaction(cuenta);
             break;
             default:
-                    System.out.println("Invalid option:\n\n"); 
+                    System.out.println("Invalid option:\n\n");
                     anotherTransaction(cuenta);
             break;
         }
@@ -111,5 +111,5 @@ public class AtmUK {
         }
     }
 
-    
+
 }
